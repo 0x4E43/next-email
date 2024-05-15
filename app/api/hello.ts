86c17e2e50683b33531bb3044492788a26cc93b1
0x4E43 <nimaic.dev@gmail.com>
2024-05-15 21:49:34 +0530

    first commit

---
+++ app/api/hello.ts
@@ -0,0 +1,18 @@
+// import type { NextApiRequest, NextApiResponse } from 'next'
+// import { NextRequest, NextResponse } from 'next/server';
+ 
+// type ResponseData = {
+//   message: string
+// }
+ 
+// export default function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<ResponseData>
+// ) {
+//   res.status(200).json({ message: 'Hello from Next.js!' })
+// }
+
+// export async function GET(request :NextRequest) {
+//   // Do whatever you want
+//   return NextResponse.json({ message: "Hello World" }, { status: 200 });
+// }
